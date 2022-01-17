3dd032f9a952045cdd40bbb082810fc030412209
Hugo Rouvier <hugo.rouvier-boutet@efrei.net>
2022-01-17 16:12:09 +0100

    Test

--- main.py
+++ main.py
@@ -11,6 +11,6 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('gros fdp')
+    print_hi('gros fdp, bienvenue dans ta mère')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
